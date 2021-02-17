7f1c33ebb3bbde4cff108280ab841e98a8861862
Saif807380 <saif1204kazi@gmail.com>
2021-02-17 22:10:37 +0530

    Had to run black, remove flake8 errors

--- src/app/middleware/auth.py
+++ src/app/middleware/auth.py
@@ -1,18 +1,23 @@
-from fastapi import HTTPException, status, Header, Depends
-from jose import JWTError, jwt
-from sqlalchemy.orm import Session
-from database.db import get_db
 import os
 
+from database.db import get_db
 from exceptions.admin import token_exception
+from fastapi import Depends
+from fastapi import Header
+from jose import jwt
+from jose import JWTError
+from sqlalchemy.orm import Session
 
-async def is_authenticated(authorization: str = Header(...), db: Session = Depends(get_db)):
+
+async def is_authenticated(
+    authorization: str = Header(...), db: Session = Depends(get_db)
+):
     token: str = authorization.split()[-1]
     try:
         payload = jwt.decode(
             token,
             os.environ.get("SECRET_KEY"),
-            algorithms=os.environ.get("ALGORITHM")
+            algorithms=os.environ.get("ALGORITHM"),
         )
         return payload.get("id")
     except JWTError:
