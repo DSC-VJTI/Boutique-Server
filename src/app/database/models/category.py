8e159b4927557a3f3854244c35ed8713cdf8d707
Saif807380 <saif1204kazi@gmail.com>
2021-04-04 17:53:09 +0530

    added product, category, sub-category models

---
+++ src/app/database/models/category.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
+
+from ..db import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+
+
+class SubCategory(Base):
+    __tablename__ = "sub_categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    category_id = Column(Integer, ForeignKey("category.id"))
+
+    category = relationship(
+        Category,
+        backref=backref("sub_categories", cascade="delete,all", lazy="joined"),
+    )
---
+++ src/app/database/models/product.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
+
+from ..db import Base
+from .category import Category
+from .category import SubCategory
+
+product_sub_category_association_table = Table(
+    "products_sub_categories",
+    Base.metadata,
+    Column("product_id", Integer, ForeignKey("products.id")),
+    Column("sub_category_id", Integer, ForeignKey("sub_categories.id")),
+)
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+
+    category_id = Column(Integer, ForeignKey("category.id"))
+
+    category = relationship(
+        Category, backref=backref("products", lazy="joined")
+    )
+
+    sub_categories = relationship(
+        SubCategory,
+        secondary=product_sub_category_association_table,
+        backref=backref("products", lazy="joined"),
+    )
