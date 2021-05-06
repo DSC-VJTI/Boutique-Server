d4c220c062c7070163f93a987c6492282cb0d7b9
Parth <parthshah576@gmail.com>
2021-05-06 10:55:47 +0530

    fix flake8 errors

--- src/app/main.py
+++ src/app/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origin_regex='^https://deploy-preview-[0-9]+--client-boutique\.netlify\.app$',
+    allow_origin_regex='^https://deploy-preview-[0-9]+--client-boutique.netlify.app$',  # noqa: E501
 )
 
 
