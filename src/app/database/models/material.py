b797c4a1af0f1ffc289afc1c270318232035adac
Saif807380 <saif1204kazi@gmail.com>
2021-05-20 12:10:58 +0530

    updated materials schema

--- src/app/database/models/material.py
+++ src/app/database/models/material.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column
+from sqlalchemy import Date
 from sqlalchemy import Integer
 from sqlalchemy import String
 
@@ -10,6 +11,8 @@ class Material(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     client_name = Column(String)
+    created_on = Column(Date)
+    last_updated = Column(Date)
 
     top = Column(String)
     bottom = Column(String)
